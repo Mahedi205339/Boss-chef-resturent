1046d57936b38eddaf383ec1e876a0da7644d473
mahedi205339 <mahedi205339gmaol.com>
2023-11-14 22:41:31 +0600

    added signUp page with react hook form

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,52 +1,26 @@
+//import { LoadCanvasTemplate } from 'react-simple-captcha';
 import './signUp.css'
-import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
-import logImg from '../../assets/others/authentication1.png'
-import { useContext, useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../../providers/AuthProvider';
-const Login = () => {
-    const captchaRef = useRef(null)
-    const [disabled, setDisabled] = useState(true);
-    const { signIn } = useContext(AuthContext)
-    useEffect(() => {
-        loadCaptchaEnginge(6)
-    }, [])
-    const handleSingUp = event => {
-        event.preventDefault();
-        const form = event.target;
-        
-        const email = form.email.value;
-        const name = form.name.value;
-        const password = form.password.value;
-        console.log(email, password)
-        signIn(email , password)
-        .then(result=>{
-            const user = result.user;
-            console.log(user)
-        })
+import logImg from '../../assets/others/authentication2.png'
+import { useForm } from 'react-hook-form';
+
+const SignUp = () => {
+    const {
+        register, handleSubmit, formState: { errors },
+    } = useForm()
+
+    const onSubmit = (data) => {
+        console.log(data)
     }
-
-    const handleValidateCaptcha = () => {
-        const userCaptchaValue = captchaRef.current.value;
-        if (validateCaptcha(userCaptchaValue)) {
-            setDisabled(false)
-        } else {
-            setDisabled(true)
-        }
-
-
-    }
-
-
 
     return (
-        <div className="signUp-back hero min-h-screen bg-base-200">
+        <div className="sign-back hero min-h-screen bg-base-200">
             <div className="hero-content flex flex-col md:flex-row">
                 <div className="text-center md:w-1/2 lg:text-left">
                     <img src={logImg} alt="" />
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm lg:w-1/2">
-                    <form onSubmit={handleSingUp} className="card-body">
+                    <form onSubmit={handleSubmit(onSubmit)} className="card-body">
                         <div>
                             <h2 className='text-center text-2xl md:text-4xl lg:text5xl font-bold text-[#d1a054]'>SignUp Now</h2>
                         </div>
@@ -54,40 +28,45 @@ const Login = () => {
                             <label className="label">
                                 <span className="label-text">Name</span>
                             </label>
-                            <input type="text" placeholder="name"
+                            <input {...register("name", { required: true })}
+                                type="text" placeholder="name"
                                 name="name"
-                                className="input input-bordered" required />
+                                className="input input-bordered"
+
+                            />
+                            {errors.name && <span className='text-red-700 font-bold'>Name is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="email" placeholder="email"
+                            <input  {...register("email", { required: true })} type="email" placeholder="email"
                                 name="email"
-                                className="input input-bordered" required />
+                                className="input input-bordered" />
+                            {errors.name && <span className='text-red-700 font-bold'>Email is required</span>}
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="password" placeholder="password"
+                            <input  {...register("password", { required: true, minLength: 8, maxLength: 20 })} type="password" placeholder="password"
                                 name="password"
-                                className="input input-bordered" required />
-
+                                className="input input-bordered" />
+                            {errors.name && <span className='text-red-700 font-bold'>Password is required</span>}
                         </div>
 
-                        <div className="form-control">
+                        {/* <div className="form-control">
                             <label className="label">
                                 <LoadCanvasTemplate></LoadCanvasTemplate>
                             </label>
-                            <input ref={captchaRef} type="text" placeholder="type the text above"
+                            <input type="text" placeholder="type the text above"
                                 name="captcha"
                                 className="input input-bordered" />
-                            <button onClick={handleValidateCaptcha} className='btn btn-xs btn-outline  mt-3'>Validate</button>
+                            <button className='btn btn-xs btn-outline  mt-3'>Validate</button>
+                        </div> */}
 
-                        </div>
                         <div className="form-control mt-6">
-                            <input disabled={disabled} className="w-full bg-[#d1a054] hover:bg-[#9c6535] py-2 rounded-lg text-white" type="submit" />
+                            <input className="w-full bg-[#d1a054] hover:bg-[#9c6535] py-2 rounded-lg text-white" type="submit" />
 
 
                         </div>
@@ -99,4 +78,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default SignUp;
